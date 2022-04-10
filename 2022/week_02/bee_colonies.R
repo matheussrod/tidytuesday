00e64600bc79f9577b2a634b5a2b45b8bd2420f7
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-04-09 22:13:34 -0300

    2022-02 Select main questions

---
+++ 2022/week_02/bee_colonies.R
@@ -0,0 +1,16 @@
+
+
+# Sources -------------------------------------------------------------------------------------------
+ 
+# - https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-01-11
+# - https://beeinformed.org/2021/06/21/united-states-honey-bee-colony-losses-2020-2021-preliminary-results/
+# - https://ocm.auburn.edu/newsroom/news_articles/2021/06/241121-honey-bee-annual-loss-survey-results.php
+# - https://www.theguardian.com/environment/2019/jun/19/us-beekeepers-lost-40-of-honeybee-colonies-over-past-year-survey-finds
+
+
+# Main questions ------------------------------------------------------------------------------------
+
+# - Are loss rates in the US similar to rates in other states?
+# - Are stressors in other states the same as in the United States?
+
+
