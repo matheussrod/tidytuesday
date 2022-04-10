23661c6357a3352f93cf57d90edb9a7d8d2a9fb0
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-04-09 22:16:19 -0300

    2022-02 Manipulate bee colonies data
    Please enter the commit message for your changes. Lines starting

--- 2022/week_02/bee_colonies.R
+++ 2022/week_02/bee_colonies.R
@@ -14,3 +14,32 @@
 # - Are stressors in other states the same as in the United States?
 
 
+# Bee colonies data ---------------------------------------------------------------------------------
+
+tuesdata <- tidytuesdayR::tt_load(2022, week = 2)
+colony <- tuesdata$colony
+stressor <- tuesdata$stressor
+
+
+## Getting information only for other states and the United States ----------------------------------
+
+states <- c("Other States", "United States")
+
+columns_to_remove <- dplyr::all_of(c("colony_n", "colony_max", "colony_lost", "colony_added", "colony_reno"))
+
+colony_states <- dplyr::filter(colony, state %in% states) |> 
+  dplyr::select(-columns_to_remove)
+
+# Getting only the main stressor for each year, month and state
+stressor_states <- dplyr::filter(stressor, state %in% states) |> 
+  dplyr::filter(!is.na(stress_pct)) |>
+  dplyr::arrange(year, months, state, dplyr::desc(stress_pct)) |> 
+  dplyr::group_by(year, months, state) |> 
+  dplyr::summarise(stress_pct = max(stress_pct),
+                   stressor = dplyr::first(stressor)) |> 
+  dplyr::ungroup()
+
+colony_stress <- colony_states |> 
+  dplyr::inner_join(stressor_states, by = c("year", "months", "state"))
+
+
