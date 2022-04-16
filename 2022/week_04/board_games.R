9cca603460643125efaeb039711fd27520ec629b
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-04-16 20:03:59 -0300

    2022-04 Manipulate board games data

--- 2022/week_04/board_games.R
+++ 2022/week_04/board_games.R
@@ -12,3 +12,19 @@
 # Is play time related to the minimum age?
 
 
+# Board games data ----------------------------------------------------------------------------------
+
+tuesdata <- tidytuesdayR::tt_load(2022, week = 4)
+
+details <- tuesdata$details
+ratings <- tuesdata$ratings
+
+details_time <- details |> 
+  dplyr::group_by(year = yearpublished) |> 
+  dplyr::summarise(min_play = mean(minplaytime),
+                   quantity = dplyr::n()) |> 
+  dplyr::ungroup() |> 
+  dplyr::filter(year >= 2001, year <= 2022)
+
+
+# Inspirations --------------------------------------------------------------------------------------
