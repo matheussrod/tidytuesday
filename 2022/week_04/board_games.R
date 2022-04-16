1ee93c4b49d3390fb9a2b370b9082df1858ea082
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-04-16 20:03:01 -0300

    2022-04 Select main questions

---
+++ 2022/week_04/board_games.R
@@ -0,0 +1,14 @@
+
+
+# Sources -------------------------------------------------------------------------------------------
+
+# https://boardgamegeek.com/
+# https://www.kaggle.com/datasets/jvanelteren/boardgamegeek-reviews
+
+
+# Main questions ------------------------------------------------------------------------------------
+
+# Does the maximum and minimum play time decrease over time?
+# Is play time related to the minimum age?
+
+
