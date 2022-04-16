cb1714c2eddb8677893b963b76a15282073cc228
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-04-15 21:28:47 -0300

    2022-03 Select main questions

---
+++ 2022/week_03/chocolate.R
@@ -0,0 +1,19 @@
+
+
+# Sources -------------------------------------------------------------------------------------------
+
+# http://flavorsofcacao.com/chocolate_database.html
+#  Rating Scale
+# 
+#  - 4.0 - 5.0  = Outstanding
+#  - 3.5 - 3.9  = Highly Recommended
+#  - 3.0 - 3.49 = Recommended
+#  - 2.0 - 2.9  = Disappointing
+#  - 1.0 - 1.9  = Unpleasant
+
+
+# Main questions ------------------------------------------------------------------------------------
+
+# - What most memorable characteristics are present in the best ratings?
+
+
