da2dcc574a5c30dcb3792dd9be99ac33129f2aa2
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-04-21 19:18:13 -0300

    2022-05 Add code for plot

--- 2022/week_05/dog_breeds.R
+++ 2022/week_05/dog_breeds.R
@@ -34,3 +34,46 @@ breed_details <- breed_characteristic |>
 # Inspirations --------------------------------------------------------------------------------------
 
 
+# Plot ----------------------------------------------------------------------------------------------
+
+library(ggplot2)
+library(ggchicklet)
+library(showtext)
+
+font_add_google("Ubuntu Mono", "Ubuntu Mono")
+
+f1 <- "Ubuntu Mono"
+f3 <- "Ubuntu Mono"
+
+showtext_auto()
+
+plot <- ggplot(breed_details, aes(forcats::fct_reorder(characteristic, percent), y = percent)) + 
+  geom_chicklet(aes(y = 1), radius = grid::unit(0.75, 'lines'), fill = "white", color = "grey90") + 
+  geom_chicklet(radius = grid::unit(0.75, 'lines'), fill = "#201E41", color = "grey90") + 
+  geom_text(aes(label = paste0(round(percent*100, 1), " %")), family = f3, hjust = 1.2, color = "white", size = 12) +
+  
+  geom_text(aes(y = 0.02, label = stringr::str_wrap(Trait_1, width = 15)), family = f3, hjust = 0, color = "white", size = 9, lineheight = 0.35) +
+  geom_text(aes(y = 1.02, label = stringr::str_wrap(Trait_5, width = 15)), family = f3, hjust = 0, color = "black", size = 9, lineheight = 0.35) +
+  
+  coord_flip(ylim = c(0, 1.2)) +
+  labs(
+    title = "Breed characteristics",
+    subtitle = "For each characteristic, 195 breeds were evaluated. The percentage represents, for the analyzed characteristic, 
+how close the evaluated breeds came to achieving the best evaluation.",
+    caption = "Source: American Kennel Club · Graphic: Matheus S. Rodrigues",
+    x = "", y = ""
+  ) +
+  theme(
+    plot.margin = margin(rep(20, 4)),
+    panel.background = element_blank(),
+    plot.title = element_text(family = f1, face = "bold", size = 120),
+    plot.subtitle = element_text(family = f3, size = 35, hjust = 0, margin = margin(t = 5, b = 20), lineheight = 0.35),
+    plot.caption = element_text(family = f3, color = "grey40", size = 30, hjust = 1),
+    plot.title.position = "plot",
+    axis.ticks = element_blank(),
+    axis.text.y = element_text(family = f3, size = 35, margin = margin(r = -10)),
+    axis.text.x = element_blank()
+  )
+
+ggsave(filename = here::here("2022/week_05/plot.png"), plot = plot, width = 10, height = 8, units = "in", dpi = 320)
+
