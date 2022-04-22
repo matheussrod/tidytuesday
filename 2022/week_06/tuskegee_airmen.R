ae3b1fb4136476da400051c58ffaf1807f580c93
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-04-22 20:07:45 -0300

    2022-06 Add code for plot

--- 2022/week_06/tuskegee_airmen.R
+++ 2022/week_06/tuskegee_airmen.R
@@ -53,3 +53,49 @@ airmen_xy <- airmen_names |>
 # Inspirations --------------------------------------------------------------------------------------
 
 
+# Plot ----------------------------------------------------------------------------------------------
+
+library(ggplot2)
+library(showtext)
+library(patchwork)
+
+font_add_google("Black Ops One", "Black Ops")
+font_add_google("Allerta Stencil", "Allerta")
+
+f1 <- "Black Ops"
+f2 <- "Allerta"
+
+showtext_auto()
+
+plot_names <- ggplot() + 
+  geom_text(data = dplyr::filter(airmen_names, x != y), aes(x = x, y = y, label = name), family = f2, color = "grey40", size = 8) + 
+  geom_text(data = airmen_xy, aes(x = x, y = y, label = name), family = f2, color = "grey40", size = 8) + 
+  xlim(c(0.5, 8.55)) + ylim(c(0.5, 8.55)) +
+  theme_void()
+
+plot <- ggplot(airmen_jets_n) + 
+  geom_segment(aes(x = group_dt, xend = group_dt, y = 0.0065, yend = -0.05), color = "grey90", linetype = "dashed") +
+  geom_point(aes(x = group_dt, y = -0.01, size = jet_n), color = "#262626", alpha = 0.50, show.legend = FALSE) + 
+  geom_text(aes(x = group_dt, y = 0.008, label = jet_n), family = f2, size = 10) +
+  labs(
+    title = "Jets shot down by Tuskegee Airmen in World War II",
+    subtitle = "In tribute of",
+    caption = "Source: CAF · Graphic: Matheus S. Rodrigues",
+    x = "", y = ""
+  ) +
+  ylim(c(-0.05, 0.05)) +
+  scale_radius(range = c(5, 50)) +
+  scale_x_date(breaks = airmen_jets_n$group_dt, labels = airmen_jets_n$group_dt, date_labels = "%m/%y") +
+  theme(
+    plot.margin = margin(rep(20, 4)),
+    panel.background = element_blank(),
+    plot.title = element_text(family = f1, size = 80, face = "bold", hjust = 0.5),
+    plot.subtitle = element_text(family = f2, color = "grey40", size = 50, hjust = 0.5, margin = margin(t = 20, b = -25)),
+    plot.caption = element_text(family = f2, size = 30, color = "grey40", hjust = 0, margin = margin(t = 10)),
+    axis.ticks = element_blank(),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(family = f2, size = 25, margin = margin(t = -20))
+  )
+
+plot2 <- plot + inset_element(plot_names, left = 0, bottom = 0.65, right = 1, top = 0.93)
+ggsave(filename = here::here("2022/week_06/plot.png"), plot = plot2, width = 10, height = 8, units = "in", dpi = 320)
